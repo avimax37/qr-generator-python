3bc74c06ebdd04d9258a6a8117d8b863fc7fa26c
Avinaba Bera <avimax37@gmail.com>
2022-08-23 00:35:18 +0530

    Init

---
+++ qrGenerator.py
@@ -0,0 +1,18 @@
+import pyqrcode
+
+from pyqrcode import QRCode
+
+print("Please enter a URL:")
+user_url = input()
+
+if user_url.find(".") != -1:
+    print("Please enter a filename:")
+    file_name = input()
+
+    url = pyqrcode.create(user_url)
+
+    url.png(f"{file_name}.png", scale = 8)
+else:
+    print("Please enter a valid URl")
+
+# if user_url contains . then valid URL otherwise invalid URL
